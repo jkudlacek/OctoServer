12a95fbc44e5ad1c6b031b1e5508476ecb71f0de
jkudlacek061 <jkudlacek061@gmail.com>
2021-01-05 15:55:35 +0100

    Buttons rework

--- server.py
+++ server.py
@@ -6,8 +6,7 @@ import json
 
 
 #### Websocket methods
-def new_client(client, server):
-    server.send_message_to_all("Hey all, a new client has joined us")
+#def new_client(client, server):
 
 def message_received(client, server, message):
     print(message)
@@ -18,44 +17,56 @@ def message_received(client, server, message):
 
     if "source" in msg:
         client["source"] = msg["source"]
-        if (msg["source"]=="js"):
-            for x in server.clients:
-                print(x)
-                if "source" in x and x["source"] == "octoprint":
-                    server.send_message(x, message)
-        # print(client["source"])
+        message_template(msg, message, "source", "js", "octoprint")
+        # if (msg["source"]=="js"):
+        #     for x in server.clients:
+        #         print(x)
+        #         if "source" in x and x["source"] == "octoprint":
+        #             server.send_message(x, message)
+
+    # print(client["source"])
 
     if "origin" in msg:
-        if msg["origin"]=="octoprint":
-            for x in server.clients:
-                print(x)
-                if "source" in x and x["source"]=="js":
-                    server.send_message(x, message)
-
-        # elif msg["origin"]=="js":
+        message_template(msg, message, "origin", "octoprint", "js")
+        # if msg["origin"]=="octoprint":
         #     for x in server.clients:
-        #         if x["source"]=="octoprint":
+        #         print(x)
+        #         if "source" in x and x["source"]=="js":
+        #             server.send_message(x, message)
+
+        message_template(msg, message, "origin", "js", "octoprint")
+        # if msg["origin"]=="js":
+        #     for x in server.clients:
+        #         print(x)
+        #         if "source" in x and x["source"]=="octoprint":
         #             server.send_message(x, message)
     else:
         print("neni")
-
-        # print(type(client["origin"]))
-
-	#print("Client(%d) said: %s" % (client['id'], message))
-
-def send_command(value):
-    server.send_message_to_all(value)
-    for x in server.clients:
-        if x["id"]==2:
-            server.send_message(x, "Testok")
 
 ###Websocket
 def server():
     global server
     server = WebsocketServer(8765, host='127.0.0.1')
-    server.set_fn_new_client(new_client)
+    #server.set_fn_new_client(new_client)
     server.set_fn_message_received(message_received)
     server.run_forever()
+
+server_thread = threading.Thread(target=server)
+server_thread.start()
+
+
+def message_template(msg, message, key, source, destination):
+    if msg[key] == source:
+        for x in server.clients:
+            print(x)
+            if "source" in x and x["source"] == destination:
+                server.send_message(x, message)
+
+# def send_command(value):
+#     server.send_message_to_all(value)
+#     for x in server.clients:
+#         if x["id"]==2:
+#             server.send_message(x, "Testik")
 
 ###TKINTER
 # class Butt:
@@ -63,16 +74,14 @@ def server():
 #         self.value=value
 
 
-def tkp():
-    master = Tk()
-    pause=Button(master, text="Pause", command=lambda: send_command("pause"))
-    pause.pack()
-    cancel = Button(master, text="Cancel", command=lambda: send_command("cancel"))
-    cancel.pack()
-    master.mainloop()
+# def tkp():
+#     master = Tk()
+#     pause=Button(master, text="Pause", command=lambda: send_command("pause"))
+#     pause.pack()
+#     cancel = Button(master, text="Cancel", command=lambda: send_command("cancel"))
+#     cancel.pack()
+#     master.mainloop()
 
-server_thread = threading.Thread(target=server)
-server_thread.start()
 
 # tk_thread = threading.Thread(target=tkp)
 # tk_thread.daemon = True
--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 let socket = new WebSocket("ws://localhost:8765");
 
 socket.onopen = function(e) {
-  //alert("[open] Connection established");
-  //alert("Sending to server");
-  // socket.send("My name is John");
-
   var msg = {
     source: "js"
   };
@@ -12,28 +8,33 @@ socket.onopen = function(e) {
 };
 
 socket.onmessage = function(event) {
-  var blbost = '<div class="field has-addons"><p class="control"><button class="button is-small">\n' +
+  var obj = JSON.parse(event.data);
+  console.log(obj);
+  if ("state" in obj){
+    $("#state").text(obj.state.text);
+    console.log(obj.job.file.name);
+    if (obj.job.file.name != null){
+      var date = new Date(obj.job.file.date*1000).toLocaleDateString("en-US");
+      $("#uploaded").text(date);
+      $("#file").text(obj.job.file.name);
+      $("#circa").text(getTime(obj.job.estimatedPrintTime)+"s");
+      $("#print_time").text(getTime(obj.progress.printTime)+"s");
+      $("#time_left").text(getTime(obj.progress.printTimeLeft)+"s");
+
+      $("#print-progress").val(obj.progress.completion);
+    }
+    disableButtons(obj.state.text);
+  };
+
+  if ("local" in obj){
+    $("#files").empty();
+    $.each(obj.local, function(key, value){
+      var file_item = '<div class="field has-addons"><p class="control"><button value="' + key + '" class="button is-small file_load">\n' +
       '    <span class="icon is-small">\n' +
       '      <i class="fas fa-print"></i>\n' +
       '    </span>\n' +
-      '  </button></p></div>'
-
-  var obj = JSON.parse(event.data);
-  console.log(obj);
-  // console.log(obj)
-  // for (var key in obj) {
-  //   if (obj.hasOwnProperty(key)) {
-  //       console.log(key + " -> " + obj[key]);
-  //   }
-  // }
-  // console.log(obj.state.text);
-  if ("state" in obj){
-    $("#state").text(obj.state.text);
-    $("#print-progress").val(obj.progress.completion);
-  };
-  if ("local" in obj){
-    $.each(obj.local, function(key, value){
-      $("#files").append("<p>" + key + "</p>" + blbost + "<hr>");
+      '  </button></p></div>';
+      $("#files").append("<p class='name'>" + key + "</p>" + file_item + "<hr>");
     });
     console.log(obj.local);
   }
@@ -42,14 +43,87 @@ socket.onmessage = function(event) {
 
 socket.onclose = function(event) {
   if (event.wasClean) {
-    //alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    console.log("Connection close clean");
   } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    //alert('[close] Connection died');
+    console.log("Connection dieded");
   }
 };
 
 socket.onerror = function(error) {
-  //alert(`[error] ${error.message}`);
+  console.log("Errorik");
 };
+
+$(document).ready(function() {
+  $(".file_load").click(function(){
+    var file_name = $(this).val();
+
+    var obj = {
+      origin: "js",
+      job: file_name
+    };
+
+    console.log(obj);
+    socket.send(JSON.stringify(obj))
+  });
+
+  $("#cancel").click(function() {
+    var obj = {
+      origin: "js",
+      cmd: "cancel"
+    };
+
+    socket.send(JSON.stringify(obj))
+  });
+
+  $("#print_toggle").click(function(){
+    var obj = {
+      origin: "js",
+      cmd: "toggle"
+    };
+
+    socket.send(JSON.stringify(obj))
+  });
+});
+
+function getTime(seconds){
+  var leftover = seconds;
+
+  var days = Math.floor(leftover / 86400);
+
+  leftover = leftover - (days * 86400);
+
+  var hours = Math.floor(leftover / 3600);
+
+  leftover = leftover - (hours * 3600);
+
+  var minutes = Math.floor(leftover / 60);
+
+  leftover = leftover - (minutes * 60);
+  leftover = Math.floor(leftover);
+  return((days >= 1 ? days + ':' : '')  + hours + ':' + minutes + ':' + leftover);
+}
+
+function disableButtons(state){
+  if (state == "Operational"){
+    $("#print_toggle").addClass("is-static");
+    $("#cancel").addClass("is-static");
+  } else if (state == "Printing") {
+    $("#print_toggle").removeClass("is-static");
+    $("#print_toggle").html('<span class="icon"><i class="fas fa-pause"></i></span>\n' + '<span>Pause</span>');
+
+    $("#cancel").removeClass("is-static");
+    $("#print").addClass("is-static");
+    $("#print").addClass("is-info");
+    $("#print").removeClass("is-danger");
+    $("#print").html('<span class="icon"><i class="fas fa-print"></i></span>\n' + '<span>Print</span>');
+
+  } else if (state == "Paused" || state =="Pausing"){
+    $("#print").removeClass("is-static");
+    $("#print").removeClass("is-info");
+    $("#print").addClass("is-danger");
+    $("#print").html('<span class="icon"><i class="fas fa-undo"></i></span>\n' + '<span>Restart</span>');
+
+    $("#print_toggle").html('<span class="icon"><i class="fas fa-play"></i></span>\n' + '<span>Resume</span>');
+
+  }
+}
